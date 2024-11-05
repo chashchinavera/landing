d47da75ec44a14ff2f9e11a1e213b3210e9f0543
Vera Chashchina <chaschinavera@gmail.com>
2024-11-05 13:08:46 +0300

    create project

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: {
+        main: './src/pages/index.js'
+    },
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    },
+
+    mode: 'development',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        open: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    'postcss-loader',
+                ]
+            },
+            {
+                test: /\.(svg|jpg)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]',
+                }
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[hash][ext]',
+                },
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ]
+}
